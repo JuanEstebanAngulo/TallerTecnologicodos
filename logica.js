9de6b79b7086642ed901027bc99ade56f356a621
Juan Esteban Angulo <juan.esteban.angulo@correounivalle.edu.co>
2024-04-08 20:56:22 -0500

    Cambio en rama pruebas

--- logica.js
+++ logica.js
@@ -2,3 +2,4 @@ alert("Bienvenidos a Taller Tecnológico dos")
 console.log("Esto que estoy escribiendo se vera en la consola HTML")
 console.log("Esta va a ser la tercera versión")
 console.log("Este cambio ira hasta remoto");
+console.log("se agrega cambio en rama pruebas")
